9615a2be3bd015cc161c283f4ffcf85348d92071
Vishnurr2k01 <vishnurr2k01@gmail.com>
2024-04-27 11:45:05 +0530

    cx_freeze

--- presetup/main.py
+++ presetup/main.py
@@ -1,17 +1,18 @@
 import subprocess
 import sys
 import os
+import subprocess,sys
+from utils import process
+from logger.logger import p
 
 def run_command(command):
     try:
-        # Run the command using Pipenv
         subprocess.run(['pipenv', 'run'] + command, check=True)
     except subprocess.CalledProcessError as e:
         print(f"Error executing command: {e}")
         sys.exit(1)
 
 def main():
-    # Check if Pipfile.lock exists
     pipfile_lock_path = os.path.join(os.getcwd(), 'Pipfile.lock')
     if not os.path.exists(pipfile_lock_path):
         print("Pipfile.lock not found. Running pipenv install...")
@@ -21,13 +22,11 @@ def main():
             print(f"Error installing dependencies: {e}")
             sys.exit(1)
 
-    # Get the command line arguments excluding the script name
     command = sys.argv[1:]
     if not command:
         print("No command provided. Using default command 'python3 execute.py'")
         command = ['python3', 'execute.py'] + sys.argv[1:]
 
-    # Run the command with Pipenv
     run_command(command)
 
 if __name__ == "__main__":
--- presetup/setup.py
+++ presetup/setup.py
@@ -1,6 +1,15 @@
-from setuptools import setup,find_packages
+from setuptools import setup, find_packages
+from cx_Freeze import setup, Executable
+
 setup(
     name="RentDrive presetup",
     version="1.0.0",
-    packages=find_packages()
+    packages=find_packages(),
+    executables=[Executable("main.py")],
+    options={
+        "build_exe": {
+            "packages": ["logger", "utils"],  # Add your package names here
+            "include_files": ["execute.py","__init__.py","utils","logger"],  # Add any additional files here
+        }
+    }
 )
